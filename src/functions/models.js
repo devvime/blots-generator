7cf89ae2aa9701b80de0deaeb94379d28183a612
Victor Alves <victor.alves@leadforce.com.br>
2023-03-08 20:19:51 -0300

    fix templates path

--- src/functions/models.js
+++ src/functions/models.js
@@ -2,23 +2,23 @@ const fs = require('fs')
 
 module.exports = {
     htmlModel: function() {
-        const file = fs.readFileSync('./src/templates/html.txt', 'utf8')
+        const file = fs.readFileSync('./node_modules/blots-generator/src/templates/html.txt', 'utf8')
         return file
     },
     scriptModel: function (fileName) {
-        const file = fs.readFileSync('./src/templates/script.txt', 'utf8')
+        const file = fs.readFileSync('./node_modules/blots-generator/src/templates/script.txt', 'utf8')
         return file.replaceAll('${fileName}', fileName)
     },
     modelComponentModel: function (fileName) {
-        const file = fs.readFileSync('./src/templates/model.txt', 'utf8')
+        const file = fs.readFileSync('./node_modules/blots-generator/src/templates/model.txt', 'utf8')
         return file.replaceAll('${fileName}', fileName)
     },
     serviceModel: function (fileName) {
-        const file = fs.readFileSync('./src/templates/service.txt', 'utf8')
+        const file = fs.readFileSync('./node_modules/blots-generator/src/templates/service.txt', 'utf8')
         return file.replaceAll('${fileName}', fileName)
     },
     sassModel: function (fileName) {
-        const file = fs.readFileSync('./src/templates/sass.txt', 'utf8')
+        const file = fs.readFileSync('./node_modules/blots-generator/src/templates/sass.txt', 'utf8')
         return file.replaceAll('${fileName}', fileName)
     }
 }
